4b605f9a866b8788590b3c51c5573426567898a5
Peter Kona <ikona23@gmail.com>
2016-06-08 03:21:13 +1200

    milliseconds

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
-
 function time() {
   var date = new Date();
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
-  var arr = [hours, minutes, seconds].map(function(num) {
+  var milliseconds  = date.getMilliseconds();
+  var arr = [hours, minutes, seconds, milliseconds].map(function(num) {
     return num < 10 ? '0' + num : String(num);
   });
 
-  hours = arr[0];
+  hours = arr[0]
   minutes = arr[1];
   seconds = arr[2];
+  milliseconds = arr[3];
 
-  return hours + minutes + seconds;
+  return hours + minutes + seconds + milliseconds;
 }
+
+console.log(time());
 
 function output(time) {
   var color = '#' + time;
   document.body.bgColor = color;
   document.body.textContent = color;
-  document.body.style.color = 'white';
-  document.body.style.fontSize = '2em';
-  document.body.style.marginLeft = '44%';
-  document.body.style.marginTop = '200px';
+  document.body.style.color = 'white'
+  document.body.style.fontSize = '2em'
+  document.body.style.marginLeft = '44%'
+  document.body.style.marginTop = '200px'
 }
 
-function startClick(callback) {
+function startClick(callback, name) {
   document.body.addEventListener('dblclick', function(event) {
     callback();
   });
 }
 
-function stopClick(callback) {
+function stopClick(callback, name) {
   document.body.addEventListener('click', function(event) {
     callback(name);
   });
